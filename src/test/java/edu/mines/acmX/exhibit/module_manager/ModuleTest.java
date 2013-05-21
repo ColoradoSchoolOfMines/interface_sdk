4faddd7a175ce73b125ccec0ad21a2b70981e520
Andrew DeMaria <ademaria@mines.edu>
2013-05-21 15:32:55 -0600

    logger support

--- src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleTest.java
+++ src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleTest.java
@@ -4,6 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import junit.framework.Assert;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Unit test for Module.
