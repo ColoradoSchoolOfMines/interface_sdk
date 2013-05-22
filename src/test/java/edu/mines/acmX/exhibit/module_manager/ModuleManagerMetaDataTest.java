4b605848d2023ce4dec7d33ceef71dc11fbcdf97
Andrew DeMaria <ademaria@mines.edu>
2013-05-22 14:55:48 -0600

    Incremental test work done

---
+++ src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleManagerMetaDataTest.java
@@ -0,0 +1,47 @@
+package edu.mines.acmX.exhibit.module_manager;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Assert;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Unit test for Module.
+ *
+ * TODO cleanup
+ * Module should have methods that can be overridden by child classes.
+ */
+public class ModuleManagerMetaDataTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ModuleManagerMetaDataTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ModuleManagerMetaDataTest.class );
+    }
+
+
+    /**
+     * Test that the module is able to specify which module should be run after
+     * it exits
+     */
+    public void testNextModule() {
+        // TODO replace with Logger
+        System.out.println("Running testNextModule");
+        assertTrue(false);
+
+    }
+}
--- src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleManagerTest.java
+++ src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleManagerTest.java
@@ -1,31 +1,64 @@
 package edu.mines.acmX.exhibit.module_manager;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.*;
 
 /**
  * Unit test for simple App.
  */
 public class ModuleManagerTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ModuleManagerTest( String testName )
-    {
-        super( testName );
+
+	@Test
+    public void testGetInstance() {
+        ModuleManager m = ModuleManager.getInstance();
+        assertTrue( m instanceof ModuleManager);
+        ModuleManager other = ModuleManager.getInstance();
+        assertTrue( m == other );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ModuleManagerTest.class );
+    // The module manager should have an instance of ModuleManagerMetaData that
+    // has been correctly instantiated with the given xml file.
+	@Test
+    public void testLoadModuleManagerConfig() {
+        String xmlPath = "module_manager/testModuleManagerManifest.xml";
+        ModuleManager m = ModuleManager.getInstance();
+        m.loadModuleManagerConfig(xmlPath);
+        ModuleManagerMetaData shouldEqual = new ModuleManagerMetaData("com.example.test");
+        assertTrue( m.getMetaData().equals( shouldEqual ));
     }
+
+    // expect a throw when the xml is baddly formed
+    @Test( expected=Exception.class )
+    public void testBadXMLModuleManagerConfig() {
+        String xmlPath = "module_manager/testBadXMLModuleManagerManifest.xml";
+        ModuleManager m = ModuleManager.getInstance();
+        m.loadModuleManagerConfig(xmlPath);
+    }
+
+    // expect a throw when an xml attribute is missing.
+    @Test( expected=Exception.class )
+    public void testBadDataModuleManagerConfig() {
+        String xmlPath = "module_manager/testBadDataModuleManagerManifest.xml";
+        ModuleManager m = ModuleManager.getInstance();
+        m.loadModuleManagerConfig(xmlPath);
+    }
+
+    // This should go through the test/resources/modules directory and get the
+    // appropriate ModuleMetaData structures from jar files.
+    @Test
+    public void testLoadAllModuleConfigs() {
+        ModuleManager m = ModuleManager.getInstance();
+        m.loadAllModuleConfigs( "modules" );
+        // TODO check that modules are loaded correctly
+        assertTrue( false );
+    }
+
+    @Test(expected = Exception.class )
+    public void testException() throws Exception {
+        throw new Exception();
+    }
+
 
 }
