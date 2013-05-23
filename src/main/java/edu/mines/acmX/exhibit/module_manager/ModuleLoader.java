7f78c0a924165a27c45bab97169e1c0955e52f3b
Andrew DeMaria <ademaria@mines.edu>
2013-05-23 09:42:32 -0600

    Added stub class for loading jars

---
+++ src/main/java/edu/mines/acmX/exhibit/module_manager/ModuleLoader.java
@@ -0,0 +1,26 @@
+package edu.mines.acmX.exhibit.module_manager;
+
+/**
+ * This class loads an instance of a module given an absolute path to the top
+ * level directory containing the jar(s) for the module(s).
+ *
+ * @author  Andrew DeMaria
+ * @author  Austin Diviness
+ */
+
+public class ModuleLoader {
+
+    // takes a path to a module jar and returns a ModuleMetaData object of the
+    // module manifest
+    /**
+     * 
+     * @param   jarPath path to the top level directory for the module jars
+     * @param   data    An instance of ModuleMetaData containing the metadata
+     *                  relevant to the desired Module to be loaded.
+     */
+    public static Module loadModule(String jarPath, ModuleMetaData data) {
+        return null;
+    }
+}
+
+
