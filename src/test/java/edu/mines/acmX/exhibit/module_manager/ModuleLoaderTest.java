67e50beaa90ac6183486acc90eaeff6fefcb365b
Andrew DeMaria <ademaria@mines.edu>
2013-05-23 09:41:35 -0600

    Added skeleton tests for desired functionality

---
+++ src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleLoaderTest.java
@@ -0,0 +1,40 @@
+package edu.mines.acmX.exhibit.module_manager;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Unit test for ModuleLoader
+ */
+public class ModuleLoaderTest {
+
+    /**
+     * This test should ensure that a proper module can be loaded given a jar
+     * path to its jar file
+     */
+    @Test
+    public void testLoadModule() {
+
+    }
+
+    /**
+     * This test should ensure that a Module is loaded in the same context as
+     * the ModuleManager so that the Singleton only has one instance.
+     */
+    @Test
+    public void testLoadModuleKeepsSingleton() {
+
+    }
+
+    /**
+     * This test ensures that all the correct and needed functions are available
+     * on the instance loaded.
+     */
+    @Test
+    public void testEnsureThatAllInterfacesExistOnLoadedInstance() {
+
+    }
+}
+
+
