d72eca7732036d7a19d931b3f15effe3651017b8
Andrew DeMaria <ademaria@mines.edu>
2013-05-23 15:44:51 -0600

    Implemented more tests

--- src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleManagerTest.java
+++ src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleManagerTest.java
@@ -1,14 +1,29 @@
 package edu.mines.acmX.exhibit.module_manager;
 
 import static org.junit.Assert.*;
-import org.junit.*;
-import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Unit test for ModuleManager
  */
-public class ModuleManagerTest 
-{
+public class ModuleManagerTest {
+
+    private class TestModule extends CommandlineModule {
+        public TestModule() {
+
+        }
+
+        @Override
+        public void init() {
+            super.init();
+        }
+    }
 
     @Before
     public void resetModuleManager() {
@@ -30,7 +45,7 @@ public class ModuleManagerTest
         String xmlPath = "module_manager/testModuleManagerManifest.xml";
         ModuleManager m = ModuleManager.getInstance();
         m.loadModuleManagerConfig(xmlPath);
-        ModuleManagerMetaData shouldEqual = new ModuleManagerMetaData("com.example.test");
+        ModuleManagerMetaData shouldEqual = new ModuleManagerMetaData("com.example.test","");
         assertTrue( m.getMetaData().equals( shouldEqual ));
     }
 
@@ -70,8 +85,8 @@ public class ModuleManagerTest
     }
 
     private ModuleMetaData createEmptyModuleMetaData(String name) {
-        Map<String, Boolean> inputTypesA = new HashMap<String, Boolean>();
-        List<String> moduleDepA = new ArrayList<String>();
+        Map<String, DependencyType> inputTypesA = new HashMap<String, DependencyType>();
+        Map<String, DependencyType> moduleDepA = new HashMap<String, DependencyType>();
         ModuleMetaData a = new ModuleMetaData(
                 name,
                 "2.3",
@@ -91,9 +106,9 @@ public class ModuleManagerTest
         ModuleMetaData a = createEmptyModuleMetaData("com.test.A");
         ModuleMetaData b = createEmptyModuleMetaData("com.test.B");
 
-        List<String> alist = new ArrayList<String>();
-        alist.add("com.test.B");
-        a.setRequiredModules(alist);
+        Map<String, DependencyType> alist = new HashMap<String, DependencyType>();
+        alist.put("com.test.B",DependencyType.REQUIRED);
+        a.setModuleDependencies(alist);
 
         ModuleManager m = ModuleManager.getInstance();
         Map<String,ModuleMetaData> modConfigs = new HashMap<String,ModuleMetaData>();
@@ -110,9 +125,9 @@ public class ModuleManagerTest
     public void testCheckModuleDependencyMissing() {
         ModuleMetaData a = createEmptyModuleMetaData("com.test.A");
 
-        List<String> alist = new ArrayList<String>();
-        alist.add("com.test.B");
-        a.setRequiredModules(alist);
+        Map<String, DependencyType> alist = new HashMap<String, DependencyType>();
+        alist.put("com.test.B",DependencyType.REQUIRED);
+        a.setModuleDependencies(alist);
 
         ModuleManager m = ModuleManager.getInstance();
         Map<String,ModuleMetaData> modConfigs = new HashMap<String,ModuleMetaData>();
@@ -125,18 +140,36 @@ public class ModuleManagerTest
     }
 
     @Test
+    public void testCheckModuleDependencyMissingWhenOptional() {
+        ModuleMetaData a = createEmptyModuleMetaData("com.test.A");
+
+        Map<String, DependencyType> alist = new HashMap<String, DependencyType>();
+        alist.put("com.test.B",DependencyType.OPTIONAL);
+        a.setModuleDependencies(alist);
+
+        ModuleManager m = ModuleManager.getInstance();
+        Map<String,ModuleMetaData> modConfigs = new HashMap<String,ModuleMetaData>();
+        modConfigs.put(a.getPackageName(), a);
+
+        m.setModuleMetaDataMap(modConfigs);
+        m.checkDependencies();
+
+        assertTrue( m.getModuleMetaDataMap().size() == 1 );
+        assertTrue( m.getModuleMetaDataMap().get(a.getPackageName()) == a);
+    }
+
+    @Test
     public void testCheckCircularModuleDependencies() {
         ModuleMetaData a = createEmptyModuleMetaData("com.test.A");
         ModuleMetaData b = createEmptyModuleMetaData("com.test.B");
 
-        List<String> alist = new ArrayList<String>();
-        alist.add("com.test.B");
-        a.setRequiredModules(alist);
-
-        List<String> blist = new ArrayList<String>();
-        blist.add("com.test.A");
-        b.setRequiredModules(blist);
-
+        Map<String, DependencyType> alist = new HashMap<String, DependencyType>();
+        alist.put("com.test.B",DependencyType.REQUIRED);
+        a.setModuleDependencies(alist);
+        
+        Map<String, DependencyType> blist = new HashMap<String, DependencyType>();
+        blist.put("com.test.A",DependencyType.REQUIRED);
+        b.setModuleDependencies(blist);
 
         ModuleManager m = ModuleManager.getInstance();
         Map<String,ModuleMetaData> modConfigs = new HashMap<String,ModuleMetaData>();
@@ -154,13 +187,13 @@ public class ModuleManagerTest
         ModuleMetaData a = createEmptyModuleMetaData("com.test.A");
         ModuleMetaData b = createEmptyModuleMetaData("com.test.B");
 
-        List<String> alist = new ArrayList<String>();
-        alist.add("com.test.B");
-        a.setRequiredModules(alist);
-
-        List<String> blist = new ArrayList<String>();
-        blist.add("com.test.C");
-        b.setRequiredModules(blist);
+        Map<String, DependencyType> alist = new HashMap<String, DependencyType>();
+        alist.put("com.test.B",DependencyType.REQUIRED);
+        a.setModuleDependencies(alist);
+        
+        Map<String, DependencyType> blist = new HashMap<String, DependencyType>();
+        blist.put("com.test.C",DependencyType.REQUIRED);
+        b.setModuleDependencies(blist);
 
         ModuleManager m = ModuleManager.getInstance();
         Map<String,ModuleMetaData> modConfigs = new HashMap<String,ModuleMetaData>();
@@ -175,26 +208,76 @@ public class ModuleManagerTest
     }
 
     @Test
-    public void testModuleCanRun() {
-        // TODO
-        // may need to check order
-        assertTrue( false );
-    }
-
-    @Test
     public void testRun() {
         // TODO
         assertTrue( false );
     }
 
     @Test
-    public void testSetDefaultModule() {
+    public void testSetDefaultModuleValid() {
+        String path = "module_manager/HorseyGoodManifest.xml";
+        ModuleManager.setPathToManifest(path);
+        ModuleManager m = ModuleManager.getInstance();
+        assertEquals("com.andrew.random", m.getMetaData().getDefaultModule());
+    }
+
+    @Test(expected=Exception.class)
+    public void testSetDefaultModuleInvalid() {
+        String path = "module_manager/HorseyBadManifest.xml";
+        ModuleManager.setPathToManifest(path);
+        ModuleManager m = ModuleManager.getInstance();
+    }
+
+    // default case
+    @Test
+    public void testSetNextModuleRequired() {
+        ModuleMetaData a = createEmptyModuleMetaData("com.test.A");
+        ModuleMetaData b = createEmptyModuleMetaData("com.test.B");
+        TestModule aModule = new TestModule();
+        
+        Map<String, DependencyType> alist = new HashMap<String, DependencyType>();
+        alist.put("com.test.B",DependencyType.REQUIRED);
+        a.setModuleDependencies(alist);
+
+        ModuleManager m = ModuleManager.getInstance();
+        Map<String,ModuleMetaData> modConfigs = new HashMap<String,ModuleMetaData>();
+        modConfigs.put(a.getPackageName(), a);
+        modConfigs.put(b.getPackageName(), b);
+
+        m.setModuleMetaDataMap(modConfigs);
+        m.setCurrentModule(aModule);
+        assertTrue(m.setNextModule("com.test.B") == true);
+    }
+
+    // fails because current module tries to open module it didn't
+    // specify in its manifest
+    @Test
+    public void testSetNextModuleIllegal() {
+        ModuleMetaData a = createEmptyModuleMetaData("com.test.A");
+        ModuleMetaData b = createEmptyModuleMetaData("com.test.B");
+        TestModule aModule = new TestModule();
+        
+        Map<String,ModuleMetaData> modConfigs = new HashMap<String,ModuleMetaData>();
+        modConfigs.put(a.getPackageName(), a);
+        modConfigs.put(b.getPackageName(), b);
+
+        ModuleManager m = ModuleManager.getInstance();
+
+        m.setModuleMetaDataMap(modConfigs);
+        m.setCurrentModule(aModule);
+        assertTrue(m.setNextModule("com.test.B") == false);
+    }
+
+    // passes because optional module is preset
+    @Test
+    public void testSetNextModuleOptionalWorks() {
         // TODO
         assertTrue( false );
     }
 
+    // fails because optional module isn't present
     @Test
-    public void testSetNextModule() {
+    public void testSetNextModuleOptionalFails() {
         // TODO
         assertTrue( false );
     }
