a70548b54e34254a40a39f42f174e3518a3466ad
Andrew DeMaria <ademaria@mines.edu>
2013-05-23 19:33:16 -0600

    Two new classes
    ModuleLoadException will be used when loading jars fails.
    
    ModuleManagerManifiestLoader is bit wordy and will do what the title says

---
+++ src/main/java/edu/mines/acmX/exhibit/module_manager/ModuleLoadException.java
@@ -0,0 +1,11 @@
+package edu.mines.acmX.exhibit.module_manager;
+
+public class ModuleLoadException extends Exception {
+
+	public ModuleLoadException(String message) {
+        super( message );
+	}
+
+}
+
+
---
+++ src/main/java/edu/mines/acmX/exhibit/module_manager/ModuleManagerManifestLoader.java
@@ -0,0 +1,24 @@
+package edu.mines.acmX.exhibit.module_manager;
+
+/**
+ * This class loads a single manifest for a ModuleManager.
+ *
+ * @author  Andrew DeMaria
+ * @author  Austin Diviness
+ */
+
+public class ModuleManagerManifestLoader {
+
+    /**
+     * Creates a ModuleManagerMetaData object
+     *
+     * @param   path path to a manifest for a module manager.
+     */
+    public static ModuleManagerMetaData loadModuleManifest(String path) {
+        // TODO
+        return null;
+    }
+
+}
+
+
