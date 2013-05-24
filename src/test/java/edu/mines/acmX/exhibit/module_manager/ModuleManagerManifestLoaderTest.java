b45e7dd46f14f7539f69c585a28b9662487f6d82
Andrew DeMaria <ademaria@mines.edu>
2013-05-23 19:35:16 -0600

    Added new tests for new class

---
+++ src/test/java/edu/mines/acmX/exhibit/module_manager/ModuleManagerManifestLoaderTest.java
@@ -0,0 +1,25 @@
+package edu.mines.acmX.exhibit.module_manager;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Unit test for ModuleManagerManifestLoader.
+ *
+ */
+public class ModuleManagerManifestLoaderTest {
+
+    @Test
+    public void testLoadCorrectModuleManagerManifest() {
+        // TODO 
+        assertTrue(false);
+    }
+
+    @Test(expected=Exception.class)
+    public void testLoadBadModuleManagerManifest() {
+        // TODO
+        assertTrue(false);
+    }
+
+}
