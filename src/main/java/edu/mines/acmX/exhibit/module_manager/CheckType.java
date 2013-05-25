1f81c82cf99a9f8a2b5696f5821f3c20180dfee9
Andrew DeMaria <ademaria@mines.edu>
2013-05-24 18:42:40 -0600

    Implemented checkModuleDependencies
    However, the tests are not yet passing as the tests rely on the Singleton
    instance working correctly for the ModuleManager so im about to go do that real
    quick
    
    Also, just to be clear, checkInputDepencies has not been implemented

---
+++ src/main/java/edu/mines/acmX/exhibit/module_manager/CheckType.java
@@ -0,0 +1,11 @@
+package edu.mines.acmX.exhibit.module_manager;
+
+public enum CheckType {
+
+    CHECKED, // Module has been checked
+    DIRTY,   // Module is in the process of being checked
+    UNKNOWN  // Module has not yet been encounted
+
+}
+
+
--- src/main/java/edu/mines/acmX/exhibit/module_manager/ModuleManager.java
+++ src/main/java/edu/mines/acmX/exhibit/module_manager/ModuleManager.java
@@ -1,7 +1,10 @@
 package edu.mines.acmX.exhibit.module_manager;
 
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * TODO cleanup
@@ -96,8 +99,39 @@ public class ModuleManager {
      * those that don't have their required module dependencies.
      */
     private void checkModuleDependencies() {
-        // TODO remove ModuleMetaData information for modules that don't
-        // have required dependencies
+    	// First generate a new depth first search data instance
+    	Map<String,CheckType> depthData = generateEmptyDepthFirstSeachData();
+    	// part of a depth first search
+    	// while there are modules that have not been checked,
+    	// check them.
+        String moduleNameToCheck;
+        boolean isModuleOkay;
+    	while((moduleNameToCheck = getFirstModuleWithType(depthData, CheckType.UNKNOWN)) != null) {
+            isModuleOkay = canModuleRunWithItsDependentModules(moduleNameToCheck, depthData);
+            if( !isModuleOkay ) {
+                this.moduleConfigs.remove(moduleNameToCheck);
+            }
+        }
+    }
+    
+    /**
+     * This function is used internally for performing its checkDepencies
+     * operation
+     *
+     * @return	Returns the first module name ( or key in this case ) of a
+     *          module that has not yet been checked (CheckType.UNKNOWN).  If
+     *          there are no modules with this status then null is returned.
+     */
+    private String getFirstModuleWithType(Map<String,CheckType> depthData, CheckType desired) {
+    	Iterator<String> i = depthData.keySet().iterator();
+    	while(i.hasNext()) {
+    		String currentKey = i.next();
+    		CheckType current = depthData.get(currentKey);
+            if( current == desired ) {
+                return currentKey;
+            }
+    	}
+		return null;
     }
 
     /**
@@ -105,7 +139,7 @@ public class ModuleManager {
      * those that don't have their required input services.
      */
     private void checkModuleInputServices() {
-
+        // TODO!!!
     }
 
     /**
@@ -113,11 +147,16 @@ public class ModuleManager {
      * required modules and input services.
      */
     public void checkDependencies() {
-        // TODO make sure to check sensor dependencies first
+        checkModuleInputServices();
+        checkModuleDependencies();
     }
 
     /**
      * checks a single ModuleMetaData for its dependencies.
+     * 
+     * Notice that this will modify the private data member moduleConfigs so
+     * that an entry that does not have a required dependency will be removed
+     * from the Map.
      *
      * @param   data    ModuleMetaData to be checked
      *
@@ -127,6 +166,88 @@ public class ModuleManager {
         // TODO check data to ensure dependencies exist
         // TODO does this check both module dependencies and sensor dependencies?
         return false;
+    }
+
+    /**
+     * This function is used internally to generate a the data needed in the
+     * depth first search algorithm.  It is not be used outside of the class.
+     *
+     * @return  An empty Map of module names to their current check status
+     */
+    private Map<String,CheckType> generateEmptyDepthFirstSeachData() {
+    	Map<String,CheckType> depthData = new HashMap<String,CheckType>();
+        Iterator<String> i = this.moduleConfigs.keySet().iterator();
+        while( i.hasNext()) {
+        	depthData.put(i.next(), CheckType.UNKNOWN);
+        }
+        
+        return depthData;
+
+    }
+
+    /**
+     * Checks a module can run only in regards to whether or not modules it
+     * depends on exist.  In essence this funciton will return false if a module
+     * has a required dependency and whether that required module and any of
+     * that referenced modules referenced modules (i.e. its recursive) do not
+     * exist.  It will return true otherwise.  Note that optional module
+     * dependencies are exactly that, optional and are not checked.
+     *
+     * Notice that this function essenitally performs a <a
+     * href="http://en.wikipedia.org/wiki/Depth-first_search">DepthFirstSeach</a>
+     * when checking modules to successfully accomplish its goal without ending
+     * in an infinite loop. 
+     *
+     * @param   current         ModuleMetaData currently being checked
+     *
+     * @param   checkedModules  Data about the current progress of the module
+     *                          checks
+     * @return                  true if the module dictated by the given current
+     *                          module name can run.
+     */
+    private boolean canModuleRunWithItsDependentModules(
+            String current,
+            Map<String,CheckType> checkedModules) {
+
+        // This is the main part of the DFS algorithm
+        checkedModules.put(current,CheckType.DIRTY);
+        ModuleMetaData meta = moduleConfigs.get(current);
+        if( meta == null ) {
+            // we ran into a module that does not exist! AHAHA... make sure
+            // that this module does not exist, stop processing and return false
+            checkedModules.remove(current);
+            moduleConfigs.remove(current); // slightly unnecessary at this point since we know it does not exist
+            return false;
+        }
+
+        // Now make sure that all required submodules exist
+        boolean moduleOkay = true;
+        Map<String, DependencyType> dependencies = meta.getModuleDependencies();
+        
+        Iterator<String> i = dependencies.keySet().iterator();
+        while( i.hasNext()) {
+        	String nextToCheck = i.next();
+        	if( dependencies.get(i) == DependencyType.REQUIRED ) {
+                // notice that the following operator will short circuit and
+                // nextToCheck may be checked later (or not at all) if
+                // moduleOkay is already false.
+                moduleOkay = moduleOkay &&
+                    canModuleRunWithItsDependentModules(nextToCheck,
+                            checkedModules); 
+        	}
+        }
+        
+        if( !moduleOkay ) {
+        	// we ran into a module that does not have all of its dependencies!
+        	// remove this module from our module listing
+        	checkedModules.remove(current);
+        	moduleConfigs.remove(current);
+        	return false;
+        } else {
+        	checkedModules.put(current, CheckType.CHECKED); // part of DFS
+        	return true;
+        }
+
     }
 
     /**
