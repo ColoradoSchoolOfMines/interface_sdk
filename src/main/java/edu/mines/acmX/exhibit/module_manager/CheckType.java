507e68b9d82c71516dfbfe3a727b95864d03ce65
Austin Diviness <austin.diviness@gmail.com>
2013-05-28 18:53:10 -0600

    added some more documentation

--- src/main/java/edu/mines/acmX/exhibit/module_manager/CheckType.java
+++ src/main/java/edu/mines/acmX/exhibit/module_manager/CheckType.java
@@ -1,10 +1,17 @@
 package edu.mines.acmX.exhibit.module_manager;
 
+/**
+ * Enum used by ModuleManager in its checkDependencies function. Used to
+ * differentiate between modules that have already been checked or not.
+ *
+ * @author	Andrew DeMaria
+ * @author	Austin Diviness
+ */
 public enum CheckType {
 
     CHECKED, // Module has been checked
     DIRTY,   // Module is in the process of being checked
-    UNKNOWN  // Module has not yet been encounted
+    UNKNOWN  // Module has not yet been encountered
 
 }
 
--- src/main/java/edu/mines/acmX/exhibit/module_manager/DependencyType.java
+++ src/main/java/edu/mines/acmX/exhibit/module_manager/DependencyType.java
@@ -1,14 +1,15 @@
 package edu.mines.acmX.exhibit.module_manager;
 
 /**
- * enum for declaring if a module/input service dependency is required
- * or optional
+ * Enum for declaring if a module/input service dependency is required
+ * or optional.
  * 
  * @author	Andrew DeMaria
  * @author	Austin Diviness
  */
 public enum DependencyType {
-    REQUIRED, OPTIONAL
+    REQUIRED,	// Module is required
+	OPTIONAL	// Module is optional
 }
 
 
