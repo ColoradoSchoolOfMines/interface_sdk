f8bdec8768c17b02b7798704d5a5737a9a8fc32c
Aakash Shah <ashah@mines.edu>
2013-05-30 13:08:34 -0600

    Added changes to include dimension from depth interface

--- src/main/java/edu/mines/acmX/exhibit/input_services/hardware/devicedata/DepthImageInterface.java
+++ src/main/java/edu/mines/acmX/exhibit/input_services/hardware/devicedata/DepthImageInterface.java
@@ -4,4 +4,7 @@ import java.nio.ShortBuffer;
 
 public interface DepthImageInterface extends DeviceDataInterface {
 	public ShortBuffer getDepthImageData();
+	
+	public int getWidth();
+	public int getHeight();
 }
