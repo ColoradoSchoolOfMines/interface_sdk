904812a9749aec71666985bcd56664c3449dda03
Aakash Shah <ashah@mines.edu>
2013-05-30 11:42:41 -0600

    Refactored to actually be a singleton now

--- src/main/java/edu/mines/acmX/exhibit/input_services/openni/OpenNIContextSingleton.java
+++ src/main/java/edu/mines/acmX/exhibit/input_services/openni/OpenNIContextSingleton.java
@@ -1,22 +1,15 @@
 package edu.mines.acmX.exhibit.input_services.openni;
 import org.OpenNI.*;
 public class OpenNIContextSingleton {
-    private static final OpenNIContextSingleton instance = new OpenNIContextSingleton();
-    private static Context context;
+    private static Context context = null;
     private static OutArg<ScriptNode> scriptNode;
     private static final String SAMPLE_XML_FILE = "openni_config.xml";
 
-    private OpenNIContextSingleton() {
-        scriptNode = new OutArg<ScriptNode>();
-        try {
-            context = Context.createFromXmlFile(SAMPLE_XML_FILE, scriptNode);
-        } catch(GeneralException ex) {
-            System.out.println(ex);
-            System.exit(1);
-        }
-    }
-
-    public static Context getContext() {
+    public static Context getContext() throws GeneralException {
+    	if (context == null) {
+    		scriptNode = new OutArg<ScriptNode>();
+    		context = Context.createFromXmlFile(SAMPLE_XML_FILE, scriptNode);    		
+    	}
         return context;
     }
 }
