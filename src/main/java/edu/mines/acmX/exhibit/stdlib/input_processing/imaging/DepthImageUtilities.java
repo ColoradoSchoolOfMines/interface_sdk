e2ddf751d842fc9ba609eebd59085178b0264352
Aakash Shah <ashah@mines.edu>
2013-06-03 09:50:15 -0600

    Refactored depth image utilities to mimick code from openni sample

--- src/main/java/edu/mines/acmX/exhibit/stdlib/input_processing/imaging/DepthImageUtilities.java
+++ src/main/java/edu/mines/acmX/exhibit/stdlib/input_processing/imaging/DepthImageUtilities.java
@@ -1,6 +1,8 @@
 package edu.mines.acmX.exhibit.stdlib.input_processing.imaging;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
 import java.nio.ShortBuffer;
 
 /**
@@ -17,18 +19,49 @@ public class DepthImageUtilities {
 	
 	public static BufferedImage shortBuffToImage(ShortBuffer sb,
 			int width, int height) {
-
-
+		
+		byte[] imgBytes = new byte[width * height];
+		float[] histogram = calcHistogram(sb, width, height);
+		
+		sb.rewind();
+		while (sb.remaining() > 0) {
+			int pos = sb.position();
+			short pixel = sb.get();
+			imgBytes[pos] = (byte) histogram[pixel];
+		}
+		
+		DataBufferByte dataBuffer = new DataBufferByte(imgBytes, width*height);
+        Raster raster = Raster.createPackedRaster(dataBuffer, width, height, 8, null);
         BufferedImage bimg = new BufferedImage( width, height, BufferedImage.TYPE_BYTE_GRAY );
-
-        while(sb.remaining() > 0)
-        {
-            int pos = sb.position();
-            short pixel = sb.get();
-            bimg.setRGB( pos%width, pos/width, pixel*16 );
-        }
-
+        bimg.setData(raster);
+        
         return bimg;
 	}
+	
+	public static float[] calcHistogram(ShortBuffer sb, int width, int height) {
+		float[] histogram = new float[width*height];
+		sb.rewind();
+		
+		int points = 0;
+		while (sb.remaining() > 0) {
+			short depthVal = sb.get();
+			if (depthVal != 0) {
+				histogram[depthVal]++;
+				points++;
+			}
+		}
+		
+		for (int i = 1; i < histogram.length; ++i) {
+			histogram[i] += histogram[i-1];
+		}
+		
+		if (points > 0) {
+			for (int i = 1; i < histogram.length; ++i) {
+				histogram[i] = (int)(256 * (1.0f - (histogram[i] / (float) points)));
+			}
+		}
+		return histogram;
+	}
+	
 
 }
