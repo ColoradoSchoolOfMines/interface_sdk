e6cb0691908eb6772f53962b5277d79f2c7f585f
Andrew DeMaria <ademaria@mines.edu>
2013-06-27 20:11:15 -0600

    Changed the default behavior to not download repositories and more user options

--- install/interfacesdksetup.sh
+++ install/interfacesdksetup.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
 
-# This script clones the proper github repositories to run the interface sdk and other major modules (such as the homescreen) 
+# This script clones the proper github repositories to run the interface sdk and
+# other major modules (such as the homescreen) 
+
+# if the --with-repos option is specified, the git repos will be downloaded and the most recent
+# version of the interface sdk will be downloaded.  Otherwise the interface sdk
+# specified in the script will be downloaded
+
+# set the default options
+repos=false
+
+for arg in "$@"
+do
+  case $arg in
+    --no-repos) repos=false;;
+    --with-repos) repos=true;;
+    --help | -h) cat << EOF
+usage: ./interfacesdksetup [--with-repos | --no-repos] [--help | -h]
+
+The following options are useful for developers not wanting to have to track
+down all the repositories.  It does not affect the runtime of the interface sdk
+
+--with-repos : downloads the source git repositories
+--no-repos   : does not download the source respositories
+
+Please report bugs to the github page at 
+https://github.com/ColoradoSchoolOfMines/interface_sdk.git
+EOF
+exit 1;;
+esac
+done
+
 
 exhibit_dir=`pwd`/exhibit
 exhibit_source_dir=`pwd`/exhibit/source
@@ -14,6 +44,8 @@ mkdir -p "$exhibit_dir/development/modules"
 mkdir -p "$exhibit_dir/production/modules"
 mkdir -p "$exhibit_source_dir/modules"
 
+if [[ "$repos" == "true" ]];
+then
 # clone the source repos
 # these are not actually needed for running as later we will be referencing another location
 cd "$exhibit_source_dir" 
@@ -22,15 +54,21 @@ git clone "$interfacesdk_repo" "$interfaceSDKdirname"
 cd "$exhibit_source_dir/modules" 
 for module in ${module_repositories[@]}
 do
-	echo "Cloning $module"
-	git clone $module
+  echo "Cloning $module"
+  git clone $module
 done
+fi
 
-# create a basic setup in development.  productioin is not populated as it should be tested in development first and then copied over
+# create a basic setup in development.  production is not populated as it should be tested in development first and then copied over
 # first get the most recent jar file
 # also here we reference the maven repository so that we dont have to worry about locally building the jars.
-cd "$exhibit_source_dir/$interfaceSDKdirname"
-wget -P "$exhibit_dir/development" "https://s3-us-west-2.amazonaws.com/acmx.mines.edu/release/edu/mines/acmX/exhibit/interfacesdk/`git describe --abbrev=0 | sed 's/v//'`/interfacesdk-`git describe --abbrev=0 | sed 's/v//'`-jar-with-dependencies.jar" 
+version="0.1.0"
+if [[ "$repos" == "true" ]];
+then
+  cd "$exhibit_source_dir/$interfaceSDKdirname"
+  version="`git describe --abbrev=0 | sed 's/v//'`"
+fi
+wget -P "$exhibit_dir/development" "https://s3-us-west-2.amazonaws.com/acmx.mines.edu/release/edu/mines/acmX/exhibit/interfacesdk/$version/interfacesdk-$version-jar-with-dependencies.jar" 
 
 # now get a general configuration file
 cd "$exhibit_dir/development"
