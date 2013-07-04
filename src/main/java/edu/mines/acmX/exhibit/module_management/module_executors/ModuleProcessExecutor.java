7e3b5cb6eab85384cfe8113be2846fca085d4f00
Andrew DeMaria <ademaria@mines.edu>
2013-07-04 12:22:05 -0500

    Added new skeleton class for Module Process Executor.
    Begins to address #22
    
    The aim of this class is to be able to execute modules in separate jvms

---
+++ src/main/java/edu/mines/acmX/exhibit/module_management/module_executors/ModuleProcessExecutor.java
@@ -0,0 +1,18 @@
+package edu.mines.acmX.exhibit.module_management.module_executors;
+
+
+public class ModuleProcessExecutor extends ModuleExecutor {
+
+	public ModuleProcessExecutor(String fullyQualifiedModuleName, String jarPath) {
+		super(fullyQualifiedModuleName, jarPath);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO
+	}
+
+}
+
+
