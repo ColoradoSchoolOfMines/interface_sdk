9bd629ed896c28ca2d88c2381be6b32bf12ded68
EricKoerlin <ekoerlin@mymail.mines.edu>
2014-06-02 10:43:25 -0600

    new approach is to close all frames and open new homescreen module, also added 32/64-bit fix

--- src/main/java/edu/mines/acmX/exhibit/input_services/hardware/drivers/MicrosoftSDK/KinectToolkitLibrary.java
+++ src/main/java/edu/mines/acmX/exhibit/input_services/hardware/drivers/MicrosoftSDK/KinectToolkitLibrary.java
@@ -5,8 +5,9 @@ import com.sun.jna.platform.win32.WinNT.*;
 import com.sun.jna.win32.StdCallLibrary;
 
 public interface KinectToolkitLibrary extends StdCallLibrary {
-	KinectToolkitLibrary INSTANCE = (KinectToolkitLibrary)
-			Native.loadLibrary("C:\\Program Files\\Microsoft SDKs\\Kinect\\Developer Toolkit v1.8.0\\Redist\\amd64\\KinectInteraction180_64.dll", KinectToolkitLibrary.class);
+	KinectToolkitLibrary INSTANCE = (KinectToolkitLibrary) ("32".equals(System.getProperty("sun.arch.data.model")) ?
+			Native.loadLibrary("C:\\Program Files\\Microsoft SDKs\\Kinect\\Developer Toolkit v1.8.0\\Redist\\x86\\KinectInteraction180_32.dll", KinectToolkitLibrary.class) :
+			Native.loadLibrary("C:\\Program Files\\Microsoft SDKs\\Kinect\\Developer Toolkit v1.8.0\\Redist\\amd64\\KinectInteraction180_64.dll", KinectToolkitLibrary.class));
 
 	public int NUI_USER_HANDPOINTER_COUNT = 2;
 
